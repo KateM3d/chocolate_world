443948aaacff41db83fe349879bf8d9b57ddf4c1
KateM3d <ekaterina8medvedeva@gmail.com>
2021-09-05 18:53:38 -0400

    added animation on h1

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
 gsap.from(".link", { opacity: 0, duration: 2, delay: 1, stagger: 0.6 })
 gsap.from(".chocolate", { y: 30, opacity: 0, duration: 1 })
 gsap.from(".btn", { y: 50, opacity: 0, duration: 3, delay: 3 })
+gsap.to('.header', { y: 20, ease: "bounce", duration: 2 })
